53f97f80e768682ae632934c9a80c57c4ec9a2ea
mridulganga <mridul.kepler@gmail.com>
2020-07-04 02:15:54 +0530

    update main

--- api/main.py
+++ api/main.py
@@ -1,10 +1,7 @@
 from flask import Flask, Response
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
 def catch_all(path):
-    return "some func"
-
-@app.route('/test1')
-def catch_all2(path):
-    return "some func 2"
+    return "test"
