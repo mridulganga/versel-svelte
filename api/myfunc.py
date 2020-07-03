1de64323d574545f3402a970b3ff2edc34cbf7d4
mridulganga <mridul.kepler@gmail.com>
2020-07-04 02:20:00 +0530

    try modifications

--- api/myfunc.py
+++ api/myfunc.py
@@ -1,7 +1,10 @@
 from flask import Flask, Response
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
+@app.route('/')
+def catch_all():
     return "test"
+
+@app.route('/test')
+def catch_all2():
+    return "test2"
